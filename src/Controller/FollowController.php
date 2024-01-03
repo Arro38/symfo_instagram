844aa16b806228c0146107d83b3f6674daa34372
arro38 <etienne.vaytilingom.pro@gmail.com>
2024-01-03 13:22:27 +0400

    Follow and Unfollow

---
+++ src/Controller/FollowController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Follow;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+
+#[Route('/api/follow')]
+class FollowController extends AbstractController
+{
+    #[Route('/add/{id}', name: 'app_follow', methods: ['POST'])]
+    public function add(EntityManagerInterface $em, User $user): JsonResponse
+    {
+        try {
+            $follow = new Follow();
+            $follower = $this->getUser();
+            $follow->setFollower($follower);
+            $follow->setFollowing($user);
+            $follow->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($follow);
+            $em->flush();
+            return new JsonResponse(['status' => 'success', 'message' => 'Followed successfully'], 200);
+        } catch (\Exception $e) {
+            return new JsonResponse(['status' => 'error', 'message' => $e->getMessage()], 500);
+        }
+    }
+
+    #[Route('/remove/{id}', name: 'app_unfollow', methods: ['POST'])]
+    public function remove(EntityManagerInterface $em, User $user): JsonResponse
+    {
+        try {
+            $follower = $this->getUser();
+            $follow = $em->getRepository(Follow::class)->findOneBy(['follower' => $follower, 'following' => $user]);
+            $em->remove($follow);
+            $em->flush();
+            return new JsonResponse(['status' => 'success', 'message' => 'Unfollowed successfully'], 200);
+        } catch (\Exception $e) {
+            return new JsonResponse(['status' => 'error', 'message' => $e->getMessage()], 500);
+        }
+    }
+
+
+    #[Route('/followers/{id}', name: 'app_followers', methods: ['GET'])]
+    public function getFollowers(EntityManagerInterface $em, User $user): JsonResponse
+    {
+        try {
+            $followers = $em->getRepository(Follow::class)->findBy(['following' => $user]);
+            $serializer = new Serializer([new ObjectNormalizer()]);
+            $jsonFollowers = $serializer->normalize($followers, 'json', [AbstractNormalizer::ATTRIBUTES => ['follower' => ['id', 'email']]]);
+
+
+            // $jsonFollowers = $serializer->serialize($followers, 'json');
+            return new JsonResponse(['status' => 'success', 'followers' => $jsonFollowers], 200);
+        } catch (\Exception $e) {
+            return new JsonResponse(['status' => 'error', 'message' => $e->getMessage()], 500);
+        }
+    }
+
+    #[Route('/followings/{id}', name: 'app_followings', methods: ['GET'])]
+    public function getFollowing(EntityManagerInterface $em, User $user): JsonResponse
+    {
+        try {
+            $followings = $em->getRepository(Follow::class)->findBy([
+                'follower' => $user
+            ]);
+            $serializer = new Serializer([new ObjectNormalizer()]);
+            $jsonFollowing = $serializer->normalize($followings, 'json', ['attributes' => ['following' => ['id', 'email']]]);
+            return new JsonResponse(['status' => 'success', 'followings' => $jsonFollowing], 200);
+        } catch (\Exception $e) {
+            return new JsonResponse(
+                ['status' => 'error', 'message' => $e->getMessage()],
+                500
+            );
+        }
+    }
+}
---
+++ src/Entity/Follow.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FollowRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+// TODO : FIX UniqueEntity does not work 
+#[UniqueEntity(fields: ['follower', 'following'], message: 'You are already following this user')]
+#[ORM\Entity(repositoryClass: FollowRepository::class)]
+class Follow
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $follower = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $following = null;
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFollower(): ?User
+    {
+        return $this->follower;
+    }
+
+    public function setFollower(?User $follower): static
+    {
+        $this->follower = $follower;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getFollowing(): ?User
+    {
+        return $this->following;
+    }
+
+    public function setFollowing(?User $following): static
+    {
+        $this->following = $following;
+
+        return $this;
+    }
+}
