cef4af2d5be93b35df5b81a227fe810c1d05fa6c
arro38 <etienne.vaytilingom.pro@gmail.com>
2024-01-05 18:25:04 +0400

    Add VichUploaderBundle and configure image upload for profile

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,19 +16,19 @@ class RegistrationController extends AbstractController
     #[Route('/api/register', name: 'app_registration')]
     public function index(UserPasswordHasherInterface $passwordHasher, Request $request, EntityManagerInterface $em): JsonResponse
     {
-
-        if ($request->isMethod('POST') && $request->getContent() !== '') {
+        if ($request->isMethod('POST')) {
+            $user = new User();
             try {
-                $data = json_decode($request->getContent(), true);
+                // get data from form data
+                $email = $request->request->get('email');
+                $password = $request->request->get('password');
+                $profilePicture = $request->files->get('profilePicture');
+
                 // validate data
-                $email = $data['email'];
-                $password = $data['password'];
-                //check if email is valid
                 if ($email === null || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
                     return new JsonResponse(['status' => 'Invalid email'], 400);
                 }
 
-                // check if password is valid
                 if (
                     $password === null || !filter_var($password, FILTER_VALIDATE_REGEXP, [
                         'options' => [
@@ -37,18 +38,25 @@ class RegistrationController extends AbstractController
                 ) {
                     return new JsonResponse(['status' => 'Invalid password'], 400);
                 }
-                $user = new User();
-                $user->setEmail($data['email']);
-                $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
+
+                if (!($profilePicture instanceof UploadedFile)) {
+                    return new JsonResponse(['status' => 'Invalid profile picture'], 400);
+                }
+                $user->setEmail($email);
+                $user->setPassword($passwordHasher->hashPassword($user, $password));
+                $user->setImageFile($profilePicture);
                 $em->persist($user);
                 $em->flush();
+
                 return new JsonResponse(['status' => 'User created!'], 201);
             } catch (\Exception $e) {
+                // remove uploaded file if error
+                if ($user->getImageName() !== null) {
+                    unlink('images/profiles/' . $user->getImageName());
+                }
                 return new JsonResponse(['status' => $e->getMessage()], 500);
-
             }
         } else {
-
             return new JsonResponse([
                 'status' => 'error',
             ], 400);
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,15 +6,24 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[Vich\Uploadable]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
+    #[Vich\UploadableField(mapping: 'profiles', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
 
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
@@ -33,6 +42,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
     }
 
     public function getEmail(): ?string
